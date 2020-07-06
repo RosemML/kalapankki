0ca9d52d6be051053cf364d0883c871bef62c9ca
Rosemari <rosemari.marttinen@gmail.com>
2020-07-06 16:48:42 +0300

    reittejä laitettu alulle

--- kalapankki/src/app/app-routing.module.ts
+++ kalapankki/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {  FishesHomeComponent } from './fishes-home/fishes-home.component';
+import { FishesDetailsComponent } from './fishes-details/fishes-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'fisheshome', component: FishesHomeComponent},
+  {path: 'fishesdetails', component: FishesDetailsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- kalapankki/src/app/app.module.ts
+++ kalapankki/src/app/app.module.ts
@@ -3,19 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FishesComponent } from './fishes/fishes.component';
 import { FishesDetailsComponent } from './fishes-details/fishes-details.component';
-import { LatestFishesComponent } from './latest-fishes/latest-fishes.component';
 import { FishesHomeComponent } from './fishes-home/fishes-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FishesComponent,
     FishesDetailsComponent,
-    LatestFishesComponent,
     FishesHomeComponent
   ],
   imports: [
